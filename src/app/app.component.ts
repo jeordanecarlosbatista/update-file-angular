738cad9a51b18b81bd8f1e83a02db7fe9746a884
jeordanecarlosbatista <jeordane.batista@hotmail.com>
2022-03-07 18:01:25 -0300

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  filesReducer: string[] = [];
+  input: any = document.querySelector('#file');
+
+  onChangeFileSelected(event: any): void {
+    console.log(this.input);
+
+    setTimeout(() => console.log(this.input), 3000);
+    console.log('onChangeFileSelected');
+    this.filesReducer = [];
+    if (this.input.multiple == true) {
+      for (let item of this.input.files) {
+        const { name } = item;
+        this.filesReducer.push(name);
+      }
+    } else {
+      let file = this.input.files.item(0);
+      this.filesReducer.push(file);
+    }
+  }
+
+  upload(): void {
+    if (this.input.multiple == true) {
+      for (let item of this.input.files) {
+        const { name } = item;
+        this.filesReducer.push(name);
+      }
+
+      // Only one file available
+    } else {
+      let file = this.input.files.item(0);
+    }
+
+    console.log(this.input);
+  }
 }
